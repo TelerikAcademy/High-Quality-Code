e56c855654d8e600dc5ad70a1043539e53bb2b9a
NikolayIT <admin@nikolay.it>
2015-03-09 17:59:58 +0200

    Thread-safe with lock Singleton example improvements

--- Design Patterns/Creational/Thread Safe Singleton/LogEvent.cs
+++ Design Patterns/Creational/Thread Safe Singleton/LogEvent.cs
@@ -1,4 +1,4 @@
-﻿namespace ThreadSafeSingleton.Logger
+﻿namespace ThreadSafeSingleton
 {
     using System;
 
--- Design Patterns/Creational/Thread Safe Singleton/LoggerWithSingleton.cs
+++ Design Patterns/Creational/Thread Safe Singleton/LoggerWithSingleton.cs
@@ -1,4 +1,4 @@
-﻿namespace ThreadSafeSingleton.Logger
+﻿namespace ThreadSafeSingleton
 {
     using System;
     using System.Collections.Generic;
