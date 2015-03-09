9f09c585407062de3bde278e8dda590e1ddfbaca
NikolayIT <admin@nikolay.it>
2015-03-09 19:58:12 +0200

    Added example of using multi-threaded singletons

--- Design Patterns/Creational/Thread Safe Singleton/Program.cs
+++ Design Patterns/Creational/Thread Safe Singleton/Program.cs
@@ -1,9 +1,13 @@
 ﻿namespace ThreadSafeSingleton
 {
+    using System.Threading.Tasks;
+
     public class Program
     {
         public static void Main()
         {
+            Parallel.For(0, 20, x => LazyThreadSafeLogger.Instance.SaveToLog(x));
+            LazyThreadSafeLogger.Instance.PrintLog();
         }
     }
 }
