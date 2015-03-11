64e41430f2e484dbb4759a6f77fccd2007eed8f0
Nikolay Kostov <admin@nikolay.it>
2015-03-11 17:18:35 +0200

    Added demos for object pooling

---
+++ Design Patterns/Creational/ObjectPool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+namespace ObjectPool
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var objectPool = new Pool();
+
+            var obj1 = objectPool.GetObject();
+            System.Console.WriteLine("Object 1 {0:MM/dd/yyyy hh:mm:ss.fff tt}", obj1.CreatedAt);
+            Thread.Sleep(1000);
+
+            var obj2 = objectPool.GetObject();
+            System.Console.WriteLine("Object 2 {0:MM/dd/yyyy hh:mm:ss.fff tt}", obj2.CreatedAt);
+            Thread.Sleep(1000);
+            
+            objectPool.ReleaseObject(obj1);
+
+            var obj3 = objectPool.GetObject();
+            System.Console.WriteLine("Object 3 {0:MM/dd/yyyy hh:mm:ss.fff tt}", obj3.CreatedAt);
+            Thread.Sleep(1000);
+        }
+    }
+}
