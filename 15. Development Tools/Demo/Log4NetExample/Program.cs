eec9ad07ad96be0b9cc6da98bc351ee6e087decf
Nikolay Kostov <admin@nikolay.it>
2015-08-06 16:46:02 +0300

    Added demo for Log4Net

---
+++ 15. Development Tools/Demo/Log4NetExample/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Log4NetExample
+{
+    using System;
+
+    using log4net;
+    using log4net.Config;
+
+    public static class Log4NetExample
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4NetExample));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            Log.Info("Info logging");
+            try
+            {
+                throw new Exception("Exception!");
+            }
+            catch (Exception e)
+            {
+                Log.Error("This is my error", e);
+            }
+
+            Console.WriteLine("[any key to exit]");
+            Console.ReadKey();
+        }
+    }
+}
