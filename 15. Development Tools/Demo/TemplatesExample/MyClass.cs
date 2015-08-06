48e603cf803466e8d6dc9eb9b6510b17728582ba
Nikolay Kostov <admin@nikolay.it>
2015-08-06 16:52:59 +0300

    Added demo for using T4 templates in Visual Studio

---
+++ 15. Development Tools/Demo/TemplatesExample/MyClass.cs
@@ -0,0 +1,18 @@
+﻿public class MyClass
+{
+    public int Item0 { get; set; }
+
+    public int Item1 { get; set; }
+
+    public int Item2 { get; set; }
+
+    public int Item3 { get; set; }
+
+    public int Item4 { get; set; }
+
+    public int Item5 { get; set; }
+
+    public void Method()
+    {
+    }
+}
---
+++ 15. Development Tools/Demo/TemplatesExample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TemplatesExample
+{
+    using System;
+    using System.IO;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var myClass = new MyClass { Item5 = 213 };
+            Console.WriteLine(myClass.Item5);
+
+            var dataFileContent = File.ReadAllText("Data.xml");
+            Console.WriteLine(dataFileContent);
+        }
+    }
+}
